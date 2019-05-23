d1fb9f05d872d482a365be0c02654414b7505df0
Lukáš Čech <ja@lukascech.cz>
2019-05-23 10:28:40 +0200

    Initial commit of the generated website to gh-pages

---
+++ assets/js/form.js
@@ -0,0 +1,12 @@
+/*global $,dataLayer*/
+var eventData = eventData || {};
+eventData.errors = eventData.errors || [];
+
+$(function () {
+  
+});
+/*
+$('#form').submit(function (event) {
+  event.preventDefault();
+});
+*/
